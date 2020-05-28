752889edd675e060d0625713af820bdee0b089a6
Andrew_Qi <qf353208262@gmail.com>
2020-05-28 15:11:19 +0800

    feat: connect to sqlite3

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	_ "UpgraderServer/routers"
+	"github.com/astaxie/beego/orm"
 
 	"github.com/astaxie/beego"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+func init()  {
+    orm.RegisterDriver("sqlite", orm.DRSqlite)
+    orm.RegisterDataBase("default", "sqlite3", "database/upgraderserver.db")
+	//orm.RunSyncdb("default", false, true)
+}
+
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
